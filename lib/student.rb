e7b8316283212e92f31940756560ef96fc474fd5
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2023-02-14 10:43:48 +0300

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge, :learn
+    def initialize
+        super
+        @knowledge=[]
+    end
 
+    def learn(learn)
+        self.knowledge << learn
+    end
 end
